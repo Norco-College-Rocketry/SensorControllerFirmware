74f293759b515efee246b9b0904abcc9c99f410a
ahthae <122731557+ahthae@users.noreply.github.com>
2025-01-28 17:24:14 -0800

    Fix error handler not running

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -552,7 +552,7 @@ void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
+//  __disable_irq();
   while (1)
   {
 	  HAL_GPIO_TogglePin(WARN_IND_GPIO_Port, WARN_IND_Pin);
